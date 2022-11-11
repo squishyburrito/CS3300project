bdb9407f4fcd4d16f02d10d5ba1a73e25cc77328
squishyburrito <cvalent3@uccs.edu>
2022-11-11 17:32:53 +0000

    Header, personal info and link to degree

---
+++ spec/example/second_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe "hello spec" do
+    # ...
+    describe String do
+      let(:string) { String.new }
+      it "should provide an empty string" do
+        expect(string).to eq("")
+      end
+    end
+   end
